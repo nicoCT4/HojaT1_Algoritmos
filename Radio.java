7e54dc2eb43395aa3beebd3f515eecef426fae5f
Fercho1118 <rue23748@uvg.edu.gt>
2024-01-14 21:22:58 -0600

    Creación de atributos y métodos abstractos
    Se agregaron los atributos de Radio y los métodos de la interfaz

--- Radio.java
+++ Radio.java
@@ -1,2 +1,81 @@
-public class Radio {
+public class Radio implements Radio_30 {
+    private boolean encendido;
+    private String marca;
+    private int banda;
+    private float estacion;
+    private int volumen;
+    private boolean apagado;
+
+    public Radio(String args) {
+
+    }
+    @Override
+    public void setEncendido(boolean encendido) {
+        this.encendido = encendido;
+    }
+
+    @Override
+    public boolean getEncendido() {
+        return encendido;
+    }
+
+    @Override
+    public void setVolumen(int volumen) {
+        this.volumen = volumen;
+    }
+
+    @Override
+    public int getVolumen() {
+        return volumen;
+    }
+
+    @Override
+    public void setEstacionBanda(float emisora, int banda) {
+        this.estacion = emisora;
+        this.banda = banda;
+    }
+
+    @Override
+    public float getEstacion() {
+        return estacion;
+    }
+
+    @Override
+    public int getBanda() {
+        return banda;
+    }
+
+    @Override
+    public void guardarEstacion(float emisora, int[] banda, int idx) {
+
+    }
+
+    @Override
+    public float recuperarEstacion(int idx) {
+        return 0.0f;
+    }
+    
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setBanda(int banda) {
+        this.banda = banda;
+    }
+
+    public void setEstacion(float estacion) {
+        this.estacion = estacion;
+    }
+
+    public void setApagado(boolean  apagado) {
+        this.apagado = apagado;
+    }
+
+    public boolean getApagado() {
+        return apagado;
+    }
 }
--- Radio_30.java
+++ Radio_30.java
@@ -1,3 +1,15 @@
 public interface Radio_30 {
-    
+
+    public static final int AM = 0;
+    public static final int FM = 1;
+
+    void setEncendido(boolean encendido);
+    void setVolumen(int volumen);
+    void setEstacionBanda(float emisora, int banda);
+    boolean getEncendido();
+    float getEstacion();
+    int getBanda();
+    int getVolumen();
+    void guardarEstacion(float emisora, int[] banda, int idx);
+    float recuperarEstacion(int idx);
 }
